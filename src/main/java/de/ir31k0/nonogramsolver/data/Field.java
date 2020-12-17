38604eda756f143208f8ac2a19b18a46a4c924eb
ir31k0 <daniel.reiko@web.de>
2020-12-17 14:54:29 +0100

    Add javadoc to the field enum

--- src/main/java/de/ir31k0/nonogramsolver/data/Field.java
+++ src/main/java/de/ir31k0/nonogramsolver/data/Field.java
@@ -1,14 +1,30 @@
 package de.ir31k0.nonogramsolver.data;
 
+/**
+ * Representation of a field of the nonogram.
+ */
 public enum Field {
-    UNKNOWN("_"), FILLED("■"), EMPTY("");
+    /** Field is unknown */
+    UNKNOWN("_"),
 
+    /** Field is filled */
+    FILLED("■"),
+
+    /** Field must be empty */
+    EMPTY("");
+
+    /** Visual character of field */
     private final String character;
 
     Field(String character) {
         this.character = character;
     }
 
+    /**
+     * Gets the visual character representation of field.
+     *
+     * @return the character as string
+     */
     public String getCharacter() {
         return character;
     }

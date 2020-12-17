5424abfa69a787f915a95bd46317ade5092187f8
ir31k0 <daniel.reiko@web.de>
2020-12-17 14:55:39 +0100

    Add javadoc to the illegal field exception class

--- src/main/java/de/ir31k0/nonogramsolver/exception/IllegalFieldException.java
+++ src/main/java/de/ir31k0/nonogramsolver/exception/IllegalFieldException.java
@@ -1,5 +1,8 @@
 package de.ir31k0.nonogramsolver.exception;
 
+/**
+ * This exception is thrown when a field conflict occurs.
+ */
 public class IllegalFieldException extends Exception {
     public IllegalFieldException(String message) {
         super(message);
